00a8b8809234ba20fe33a329e6174782a55de525
Jovani Benavides <xavibenavides@yahoo.com>
2023-03-08 19:28:02 -0800

    Add project files.

---
+++ COMP003A.FinalProject/Program.cs
@@ -0,0 +1,55 @@
+﻿/* 
+ * Author:
+ * Course:
+ * Purpose: 
+ */
+
+using System.Text.RegularExpressions;
+
+namespace COMP003A.FinalProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your first name: ");
+            string Firstname = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            string Lastname = Console.ReadLine();
+
+            if (Namecheck(Firstname))
+            {
+      
+                Console.WriteLine("Name is valid.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name.");
+            }
+            if (Namecheck(Lastname))
+            {
+
+                Console.WriteLine("Name is valid.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name.");
+            }
+        }
+        static bool Namecheck(String Name)
+        {
+            Name =Name.ToLower();
+
+            string pattern = "^[a-z]";
+            if (Regex.IsMatch(Name,pattern))
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
