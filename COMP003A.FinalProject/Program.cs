91e4b5770284855a1130684f2b7fb6e50113a524
BenaJovi <xavibenavides@yahoo.com>
2023-03-10 15:04:50 -0800

    Cooommmi

--- COMP003A.FinalProject/Program.cs
+++ COMP003A.FinalProject/Program.cs
@@ -12,35 +12,43 @@ namespace COMP003A.FinalProject
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter your first name: ");
-            string Firstname = Console.ReadLine();
-            Console.Write("Enter your last name: ");
-            string Lastname = Console.ReadLine();
+            string FirstName; string lastName; string YearBorn; string Gender;
+            int BirthYear; int age;
+            FirstName = WInputAnswer("Please enter your first name: ", "ERROR:Invalid Name.Please only use letters.");
+            lastName = WInputAnswer("Please enter your last name: ", "ERROR:Invalid Name.Please only use letters.");
 
-            if (Namecheck(Firstname))
+            if(FirstName != lastName ) 
             {
-      
-                Console.WriteLine("Name is valid.");
+                Console.WriteLine($" Welcome! {FirstName}{lastName} to BenaFit!\n We need some more info to build your profile.");
             }
-            else
-            {
-                Console.WriteLine("Invalid Name.");
-            }
-            if (Namecheck(Lastname))
-            {
 
-                Console.WriteLine("Name is valid.");
-            }
-            else
+            do
             {
-                Console.WriteLine("Invalid Name.");
+                YearBorn = NInputAnswer("Enter your the year you were born: ");
+                BirthYear = Convert.ToInt16(YearBorn);
+            } while (!checkBirthYear(BirthYear));
+            age = 2023 - BirthYear;
+            Console.WriteLine($"Looks like we are {age} this year.");
+
+            Console.Write("Enter Gender:");
+            Console.ReadLine();
+            do
+            {
+                Gender=WInputAnswer(" What is y")s
             }
         }
-        static bool Namecheck(String Name)
+        
+
+        /// <summary>
+        /// Checks Words
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <returns></returns>
+        static bool InputCheck(string Name)
         {
             Name =Name.ToLower();
 
-            string pattern = "^[a-z]";
+            string pattern = "^[A-Za-z]+$";
             if (Regex.IsMatch(Name,pattern))
             {
                 return true;
@@ -50,6 +58,87 @@ namespace COMP003A.FinalProject
                 return false;
             }
         }
+        /// <summary>
+        /// Checks Numbers
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        static bool NumberCheck(string number)
+        {
+            string pattern = "^[0-9]+$";
+            if (Regex.IsMatch(number, pattern))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Birth
+        /// </summary>
+        /// <param name="BirthYear"></param>
+        /// <returns></returns>
+        static bool checkBirthYear(int BirthYear)
+        {
+            if (BirthYear >= 1900 && BirthYear <= 2023)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Sorry that date is invalid. Pleae enter a date from 1900-2023. Try again.");
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Answers
+        /// </summary>
+        /// <param name="question"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        static string WInputAnswer(string question, string error)
+        {
+            string answer;
+            while (true)
+            {
+                Console.Write(question);
+                answer = Console.ReadLine();
+                if (InputCheck(answer))
+                {
+                    return answer;
+                }
+                else
+                {
+                    Console.WriteLine(error);
+                }
+            }
+        }
+        /// <summary>
+        /// Verifies Question answer
+        /// </summary>
+        /// <param name="question"></param>
+        /// <returns></returns>
+        static string NInputAnswer(string question)
+        {
+            string number;
+            while (true)
+            {
+                Console.Write(question);
+                number = Console.ReadLine();
+                if (NumberCheck(number))
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine("Please only use numbers.");
+                }
+            }
+        }
+
     }
 }
 
