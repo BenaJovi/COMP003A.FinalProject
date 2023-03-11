47c78349c01203634cf4e2dbc8cb3891977c2fbc
BenaJovi <xavibenavides@yahoo.com>
2023-03-11 12:49:47 -0800

    Partial

--- COMP003A.FinalProject/Program.cs
+++ COMP003A.FinalProject/Program.cs
@@ -12,14 +12,23 @@ namespace COMP003A.FinalProject
     {
         static void Main(string[] args)
         {
+            //Variables for first section
             string FirstName; string lastName; string YearBorn; string Gender;
+            char Gletter;
             int BirthYear; int age;
+            // Variables for Questions,
+            string PhysicallyActive; string DailyWalk; string LoseWeight; string random;
+            // convert to int
+            string Wheight; string Wweight;
+            
+            int FastFood; int Height; int weight; int scale; int days;
+
             FirstName = WInputAnswer("Please enter your first name: ", "ERROR:Invalid Name.Please only use letters.");
             lastName = WInputAnswer("Please enter your last name: ", "ERROR:Invalid Name.Please only use letters.");
 
             if(FirstName != lastName ) 
             {
-                Console.WriteLine($" Welcome! {FirstName}{lastName} to BenaFit!\n We need some more info to build your profile.");
+                Console.WriteLine($"Welcome {FirstName}{lastName} to BenaFit!\nWe need some more info to build your profile.");
             }
              
             do
@@ -28,14 +37,49 @@ namespace COMP003A.FinalProject
                 BirthYear = Convert.ToInt16(YearBorn);
             } while (!checkBirthYear(BirthYear));
             age = 2023 - BirthYear;
-            Console.WriteLine($"Looks like we are {age} this year.");
+            Console.WriteLine($"Looks like you are {age} this year.");
 
-            Console.Write("Enter Gender:");
-            Console.ReadLine();
             do
             {
-                Gender=WInputAnswer(" What is y")s
-            }
+                Gender = WInputAnswer(" What is your gender? Please enter (M,F, or N): ", "Please only use letters.");
+                if (InputCheck(Gender))
+                {
+                    Gender = Gender.ToLower();
+                    if (Gender == "m" || Gender == "f" || Gender == "n")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("In order to go to the next question we need to know your gender\n if you do not wish to disclose this inforamtion please select N.");
+                    }
+                }
+            } 
+            while (true);
+            Gletter=Convert.ToChar(Gender);
+
+            // Question array
+            Wheight = NInputAnswer("Please enter your height in Inches: ");
+           // Height= Convert.ToInt16(Wheight);
+            Wweight = NInputAnswer("Please enter your weight in pounds: ");
+            // weight = Convert.ToInt16(Wweight);
+            PhysicallyActive = WInputAnswer("Would you say you are physically active?", "Please only use letters.");
+            DailyWalk = WInputAnswer("Do you walk daily?", "Please only use letters.");
+            random = NInputAnswer("On average how many miles do you walk in a day?");
+            LoseWeight = WInputAnswer("Are you willing to put the effort and lose weight?", "Please only use letters.");
+            
+
+
+
+
+
+
+            string[] strArray = new string[] { Wweight, Wweight, PhysicallyActive, DailyWalk, random, LoseWeight };
+           
+            
+
+
+
         }
         
 
