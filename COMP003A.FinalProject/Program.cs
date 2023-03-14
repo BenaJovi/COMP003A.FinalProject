2fdd50d3b1cadbc121dc58908bd2671970cd42cc
BenaJovi <xavibenavides@yahoo.com>
2023-03-13 23:40:41 -0700

    FIXED VARS

--- COMP003A.FinalProject/Program.cs
+++ COMP003A.FinalProject/Program.cs
@@ -15,18 +15,18 @@ namespace COMP003A.FinalProject
             SectionIntroSeparator("Welcome to BenaFit");
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
             //Variables for first section
-            // TODO: by convention, variable names should be in camelCase except for constant or private variables
-            string FirstName; string lastName; string YearBorn; string Gender; //string TrueGender; 
-            char Gletter; // TODO: by convention, variable names should be in camelCase except for constant or private variables
+            // **TODO: by convention, variable names should be in camelCase except for constant or private variables
+            string FirstName; string lastName; string yearBorn; string gender; //string TrueGender; 
+            char gLetter; // **TODO: by convention, variable names should be in camelCase except for constant or private variables
             int BirthYear; int age; // TODO: by convention, variable names should be in camelCase except for constant or private variables
 
             // Variables for Questions,
-            string PhysicallyActive; string DailyWalk; string LoseWeight; string Wdays; // TODO: by convention, variable names should be in camelCase except for constant or private variables
+            string physicallyActive; string dailyWalk; string loseWeight; string wDays; // **TODO: by convention, variable names should be in camelCase except for constant or private variables
 
             // convert to int
-            string Wheight; string Wweight; string Wcalorieintake; string Wfastfood; string Wmiles; string Wscale; // TODO: by convention, variable names should be in camelCase except for constant or private variables
+            string wHeight; string wWeight; string wCalorieIntake; string wFastFood; string wMiles; string wScale; // **TODO: by convention, variable names should be in camelCase except for constant or private variables
 
-            int FastFood; int Height; int weight; int scale; int days; int CalorieIntake; int miles; // TODO: by convention, variable names should be in camelCase except for constant or private variables
+            int fastFood; int height; int weight; int scale; int days; int calorieIntake; int miles; // **TODO: by convention, variable names should be in camelCase except for constant or private variables
 
             //Ask the user for their name in only letters.
             FirstName = WInputAnswer("Please enter your first name: ");
@@ -38,8 +38,8 @@ namespace COMP003A.FinalProject
                 do
                 {
                     // verifies that a nummber values was entered and if the value was from 1900-2023
-                    YearBorn = NInputAnswer("Enter your the year you were born: ");
-                    BirthYear = Convert.ToInt16(YearBorn);
+                    yearBorn = NInputAnswer("Enter your the year you were born: ");
+                    BirthYear = Convert.ToInt16(yearBorn);
 
                 } while (!checkBirthYear(BirthYear));
 
@@ -49,11 +49,11 @@ namespace COMP003A.FinalProject
                 do
                 {
                     // asks for your gender and will only take m,f, or n
-                    Gender = WInputAnswer("What is your gender? Please enter (M,F, or O):");
-                    if (InputCheck(Gender))
+                    gender = WInputAnswer("What is your gender? Please enter (M,F, or O):");
+                    if (InputCheck(gender))
                     {
-                        Gender = Gender.ToLower();
-                        if (Gender == "m" || Gender == "f" || Gender == "o")
+                        gender = gender.ToLower();
+                        if (gender == "m" || gender == "f" || gender == "o")
                         {
                             break;
                         }
@@ -68,19 +68,19 @@ namespace COMP003A.FinalProject
                 } while (true);
 
                 // stores letter entered as the correct gender
-                Gletter = Convert.ToChar(Gender);
+                gLetter = Convert.ToChar(gender);
 
-                if (Gletter == 'm')
+                if (gLetter == 'm')
                 {
-                    Gender = "Male";
+                    gender = "Male";
                 }
-                else if (Gletter == 'f')
+                else if (gLetter == 'f')
                 {
-                    Gender = "Female";
+                    gender = "Female";
                 }
                 else
                 {
-                    Gender = "Other";
+                    gender = "Other";
                 }
 
                 // Question secction
@@ -90,32 +90,32 @@ namespace COMP003A.FinalProject
                 // TODO: use the questions (Questions) you have in the arrays section. remember, the variable & its value(s) should be declared before you get to use them.
                 // Question array
                 /*Q1*/
-                Wheight = NInputAnswer("Please enter your height in Inches:"); // TODO (optional): should this input allow 0 as an entry?
-                Height = Convert.ToInt16(Wheight);
+                wHeight = NInputAnswer("Please enter your height in Inches:"); // TODO (optional): should this input allow 0 as an entry?
+                height = Convert.ToInt16(wHeight);
                 /*Q2*/
-                Wweight = NInputAnswer("\nPlease enter your weight in pounds:"); // TODO (optional): should this input allow 0 as an entry?
-                weight = Convert.ToInt16(Wweight);
+                wWeight = NInputAnswer("\nPlease enter your weight in pounds:"); // TODO (optional): should this input allow 0 as an entry?
+                weight = Convert.ToInt16(wWeight);
                 /*Q3*/
-                PhysicallyActive = WInputAnswer("\nWould you say you are physically active? Answer:"); // TODO (optional): limit the responses to yes or no
+                physicallyActive = WInputAnswer("\nWould you say you are physically active? Answer:"); // TODO (optional): limit the responses to yes or no
                 /*Q4*/
-                DailyWalk = WInputAnswer("\nDo you walk daily? Answer:"); // TODO (optional): limit the responses to yes or no
+                dailyWalk = WInputAnswer("\nDo you walk daily? Answer:"); // TODO (optional): limit the responses to yes or no
                 /*Q5*/
-                Wmiles = NInputAnswer("\nOn average how many miles do you walk in a day? Answer:");
-                miles = Convert.ToInt16(Wmiles);
+                wMiles = NInputAnswer("\nOn average how many miles do you walk in a day? Answer:");
+                miles = Convert.ToInt16(wMiles);
                 /*Q6*/
-                Wfastfood = NInputAnswer("\nHow often do you eat fastfood out of the week? Answer:");
-                FastFood = Convert.ToInt16(Wfastfood);
+                wFastFood = NInputAnswer("\nHow often do you eat fastfood out of the week? Answer:");
+                fastFood = Convert.ToInt16(wFastFood);
                 /*Q7*/
-                Wcalorieintake = NInputAnswer("\nHow many calories do you eat in a day:"); // TODO (optional): should this input allow 0 as an entry?
-                CalorieIntake = Convert.ToInt16(Wcalorieintake);
+                wCalorieIntake = NInputAnswer("\nHow many calories do you eat in a day:"); // TODO (optional): should this input allow 0 as an entry?
+                calorieIntake = Convert.ToInt16(wCalorieIntake);
                 /*Q8*/
-                LoseWeight = WInputAnswer("\nIs your goal to lose weight? Answer:"); // TODO (optional): limit the responses to yes or no
+                loseWeight = WInputAnswer("\nIs your goal to lose weight? Answer:"); // TODO (optional): limit the responses to yes or no
                 /*Q9*/
-                Wdays = NInputAnswer("\nHow many days out of the week are you available to work out? Answer:");
-                days = Convert.ToInt16(Wdays);  // TODO (optional): should this input allow > 7 as an entry?
+                wDays = NInputAnswer("\nHow many days out of the week are you available to work out? Answer:");
+                days = Convert.ToInt16(wDays);  // TODO (optional): should this input allow > 7 as an entry?
 
                 // Arrays for questions and answers.
-                string[] QuestArray = new string[] { Wheight, Wweight, PhysicallyActive, DailyWalk, Wmiles, Wfastfood, Wcalorieintake, LoseWeight, Wdays }; // TODO: name this variable more appropriately (e.g., responses)
+                string[] responses = new string[] { wHeight, wWeight, physicallyActive, dailyWalk, wMiles, wFastFood, wCalorieIntake, loseWeight, wDays }; // TODO: name this variable more appropriately (e.g., responses)
                 string[] Questions = new string[] { "Please enter your height in Inches:", "Please enter your weight in pounds:","Would you say you are physically active?",
                     "Do you walk daily?","On average how many miles do you walk in a day?","How often do you eat fastfood out of the week?","How many calories do you eat in a day:",
                     "Is your goal to lose weight?","How many days out of the week are you available to work out?"}; // TODO: you should be using this data structure in the above section. otherwise, it is simply redundant
@@ -125,10 +125,10 @@ namespace COMP003A.FinalProject
                 Console.ForegroundColor = ConsoleColor.White;
 
                 // Returns the personal info that the user inputs at the beginning as well as the questions and answers.  
-                Console.WriteLine($"User:{FirstName} {lastName}\nAge:{age}\nGender:{Gender}");
+                Console.WriteLine($"User:{FirstName} {lastName}\nAge:{age}\nGender:{gender}");
 
                 // Prints the array for questions and the answers. 
-                printArray(QuestArray, Questions);
+                printArray(responses, Questions);
 
                 // Header to let the user know the profile is built and there is one more question. 
                 SectionMessageSeparator("\t\tThis will be the last question and the evaluation will  be complete.");
@@ -138,8 +138,8 @@ namespace COMP003A.FinalProject
                 /*Q10*/
                 Console.ForegroundColor = ConsoleColor.DarkMagenta;
                 Console.Write("On a scale from 1-10 (1 being poor and 10 being super healthy) how healthy do you think you are? Answer:");
-                Wscale = Console.ReadLine();
-                scale = Convert.ToInt16(Wscale); // TODO: since you are not validating this input, consider using a try-catch to handle the exception when getting a string input here
+                wScale = Console.ReadLine();
+                scale = Convert.ToInt16(wScale); // TODO: since you are not validating this input, consider using a try-catch to handle the exception when getting a string input here
 
                 // the start of the switch statement 1-10 
                 Console.ForegroundColor = ConsoleColor.Cyan;           // Switch statement will generate fitness plan based on what you feel your current health is at. 
@@ -147,42 +147,42 @@ namespace COMP003A.FinalProject
                 {
                     case 1:
                         Console.WriteLine($"\n{FirstName} here is was the team at BenaFit thinks is best for you.\nIt looks like we need to make some serious health changes." +
-                        $"Lets try excercising {Wdays} times a week.\n Right now you are at {Wweight} lets try cutting 40lbs.\nAlso, by increasing your daily miles," +
+                        $"Lets try excercising {wDays} times a week.\n Right now you are at {wWeight} lets try cutting 40lbs.\nAlso, by increasing your daily miles," +
                         $"then cutting the amount of fast food and calories you are consuming it will make it\neasier to lose weight.\nThank you {FirstName} {lastName} for using BenaFit! Please report back in 6 months. Stay healthy!");
                         break;
                     case 2:
                         Console.WriteLine($"\n{FirstName} here is was the team at BenaFit thinks is best for you.\nIt looks like we need to start taking things more serious." +
-                        $"Lets try excercising {Wdays} times a week.\nAs of now you are at {Wweight} lets try cutting 35lbs.\nAlso, by increasing your daily miles," +
+                        $"Lets try excercising {wDays} times a week.\nAs of now you are at {wWeight} lets try cutting 35lbs.\nAlso, by increasing your daily miles," +
                         $"then cutting the amount of fast food and calories\n you are consuming will make it easier to lose weight.\nThank you {FirstName} {lastName} for using BenaFit! Please report back in 6 months. Stay healthy!");
                         break;
                     case 3:
                         Console.WriteLine($"\n{FirstName} here is was the team at BenaFit thinks is best for you.\nIt looks like you are aware of your current health state and just need a push.\n" +
-                        $"So lets try excercising {Wdays} times a week. Right now you are at {Wweight} lets try cutting 30lbs.\nAlso, by increasing your daily miles," +
+                        $"So lets try excercising {wDays} times a week. Right now you are at {wWeight} lets try cutting 30lbs.\nAlso, by increasing your daily miles," +
                         $"then cutting the amount of fast food and calories you are consuming will make it\n easier to lose weight.\nThank you {FirstName} {lastName} for using BenaFit! Please report back in 6 months. Stay healthy!");
                         break;
                     case 4:
                         Console.WriteLine($"\n{FirstName} here is was the team at BenaFit thinks is best for you.\nIt looks like we need to make some serious health changes." +
-                        $"Lets try excercising {Wdays} times a week. Right now you are at {Wweight} lets try cutting 25lbs. Also, by increasing your daily miles," +
+                        $"Lets try excercising {wDays} times a week. Right now you are at {wWeight} lets try cutting 25lbs. Also, by increasing your daily miles," +
                         $"then cutting the amount of fast food and calories\n you are consuming will make it easier to lose weight.\nThank you {FirstName} {lastName} for using BenaFit! Please report back in 6 months. Stay healthy!");
                         break;
                     case 5:
                         Console.WriteLine($"\n{FirstName} here is was the team at BenaFit thinks is best for you.\nIt looks like we are right in the middle and we want to be at around 7.\n" +
-                        $"Lets try excercising {Wdays} times a week. Right now you are at {Wweight} lets try cutting 20lbs.\nIf we cut down on fast food" +
+                        $"Lets try excercising {wDays} times a week. Right now you are at {wWeight} lets try cutting 20lbs.\nIf we cut down on fast food" +
                         $"and consume healthy calories it will make it easier to lose weight. Also, it is important to stay consisent.\nThank you {FirstName} {lastName} for using BenaFit! Please report back in 3 months. Stay healthy!");
                         break;
                     case 6:
                         Console.WriteLine($"\n{FirstName} here is was the team at BenaFit thinks is best for you.\nAlright! Good job you are above average but that doesnt mean we cant improve.\n" +
-                        $"How about we train hard {Wdays} times a week. Right now you are at {Wweight} lets try cutting 10lbs.\nIts easy to go and eat fast food but lets try meal prepping." +
+                        $"How about we train hard {wDays} times a week. Right now you are at {wWeight} lets try cutting 10lbs.\nIts easy to go and eat fast food but lets try meal prepping." +
                         $"Being in control of what you eat and how much eat will\naccelerate reults and get you looking good.\nThank you {FirstName} {lastName} for using BenaFit! Please report back in 6 months. Stay healthy!");
                         break;
                     case 7:
                         Console.WriteLine($"\n{FirstName} here is was the team at BenaFit thinks is best for you.\nYou are awesome! Great job on keeping yourself healthy, this is right where we want to be.\n" +
-                        $"For you {Wdays} times a week is best and since we are at {weight} lets try cutting 5lbs.\nThe easiest way to maintain this level is" +
+                        $"For you {wDays} times a week is best and since we are at {weight} lets try cutting 5lbs.\nThe easiest way to maintain this level is" +
                         $"cutting out unhealthy food options and increasing our healthy calories.\nThank you {FirstName} {lastName} for using BenaFit! Please report back in 6 months. Stay healthy!");
                         break;
                     case 8:
                         Console.WriteLine($"\n{FirstName} here is was the team at BenaFit thinks is best for you.\nAmazing! You are very healthy.\n" +
-                        $"Since you are healthy lets stay with working out {Wdays} times a week.\nThen if you arent already watching your food intake " +
+                        $"Since you are healthy lets stay with working out {wDays} times a week.\nThen if you arent already watching your food intake " +
                         $"we suggest that you start, since its the easiet way to maintain your health.\nThank you {FirstName} {lastName} for using BenaFit! Please report back in 6 months. Stay healthy!");
                         break;
                     case 9:
